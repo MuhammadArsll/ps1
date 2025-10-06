8a43e6ff5f0235927f5c99faf825c89dce139447
MuhammadArsll <arslan2147c@gmail.com>
2025-10-06 03:04:12 -0700

    Implemented and Updated Filter lists and Filter Test methods

--- src/twitter/Filter.java
+++ src/twitter/Filter.java
@@ -3,64 +3,68 @@
  */
 package twitter;
 
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Filter consists of methods that filter a list of tweets for those matching a
- * condition.
- * 
- * DO NOT change the method signatures and specifications of these methods, but
- * you should implement their method bodies, and you may add new public or
- * private methods or classes if you like.
- */
 public class Filter {
 
     /**
      * Find tweets written by a particular user.
      * 
-     * @param tweets
-     *            a list of tweets with distinct ids, not modified by this method.
-     * @param username
-     *            Twitter username, required to be a valid Twitter username as
-     *            defined by Tweet.getAuthor()'s spec.
+     * @param tweets list of tweets with distinct ids, not modified by this method.
+     * @param username Twitter username, required to be a valid Twitter username (but
+     *                 not necessarily appearing in the list of tweets).
      * @return all and only the tweets in the list whose author is username,
      *         in the same order as in the input list.
      */
     public static List<Tweet> writtenBy(List<Tweet> tweets, String username) {
-        throw new RuntimeException("not implemented");
+        List<Tweet> result = new ArrayList<>();
+        for (Tweet tweet : tweets) {
+            if (tweet.getAuthor().equalsIgnoreCase(username)) {
+                result.add(tweet);
+            }
+        }
+        return result;
     }
 
     /**
      * Find tweets that were sent during a particular timespan.
      * 
-     * @param tweets
-     *            a list of tweets with distinct ids, not modified by this method.
-     * @param timespan
-     *            timespan
+     * @param tweets list of tweets with distinct ids, not modified by this method.
+     * @param timespan timespan
      * @return all and only the tweets in the list that were sent during the timespan,
      *         in the same order as in the input list.
      */
     public static List<Tweet> inTimespan(List<Tweet> tweets, Timespan timespan) {
-        throw new RuntimeException("not implemented");
+        List<Tweet> result = new ArrayList<>();
+        for (Tweet tweet : tweets) {
+            if (!tweet.getTimestamp().isBefore(timespan.getStart()) &&
+                !tweet.getTimestamp().isAfter(timespan.getEnd())) {
+                result.add(tweet);
+            }
+        }
+        return result;
     }
 
     /**
-     * Find tweets that contain certain words.
+     * Find tweets that contain at least one of the specified words.
      * 
-     * @param tweets
-     *            a list of tweets with distinct ids, not modified by this method.
-     * @param words
-     *            a list of words to search for in the tweets. 
-     *            A word is a nonempty sequence of nonspace characters.
-     * @return all and only the tweets in the list such that the tweet text (when 
-     *         represented as a sequence of nonempty words bounded by space characters 
-     *         and the ends of the string) includes *at least one* of the words 
-     *         found in the words list. Word comparison is not case-sensitive,
-     *         so "Obama" is the same as "obama".  The returned tweets are in the
-     *         same order as in the input list.
+     * @param tweets list of tweets with distinct ids, not modified by this method.
+     * @param words list of words to search for, case-insensitive.
+     * @return all and only the tweets in the list that contain at least one of the
+     *         words (in any case), in the same order as in the input list.
      */
     public static List<Tweet> containing(List<Tweet> tweets, List<String> words) {
-        throw new RuntimeException("not implemented");
+        List<Tweet> result = new ArrayList<>();
+        for (Tweet tweet : tweets) {
+            String text = tweet.getText().toLowerCase();
+            for (String word : words) {
+                if (text.contains(word.toLowerCase())) {
+                    result.add(tweet);
+                    break; // move to next tweet
+                }
+            }
+        }
+        return result;
     }
-
 }
